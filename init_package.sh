74f9658e8a9cc5ada324ad30015962804b5ce44f
Uki D. Lucas <UkiDLucas@gmail.com>
2024-01-02 11:39:29 -0800

    create package

---
+++ init_package.sh
@@ -0,0 +1,9 @@
+clear
+
+swift package init --name planetary_science --type executable
+#### Creating executable package: planetary_science
+#### Creating Package.swift
+#### (base) 24-01-02 11:21:27 ukidlucas @ planetary_science 
+
+#### ࠈ ls 
+#### Package.swift ...

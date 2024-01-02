5529e0d19d97ceed445ba6c68fb0976c3552a00b
Uki D. Lucas <UkiDLucas@gmail.com>
2024-01-02 11:42:34 -0800

    clean up main

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,9 @@
 import Foundation
 //import solar_system
 
+print(" Hello, Solar System!")
 
-let sun = Sun()
-print(" Hello, Solar System is  \(sun.) years old.")
+
+// let sun = Sun()
+// print(" Hello, Solar System is  \(sun.) years old.")
 

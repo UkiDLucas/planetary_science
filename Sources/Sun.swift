61b1fc439891ba8f81d1ab7e51490ada8a1d268e
Uki D. Lucas <UkiDLucas@gmail.com>
2024-01-02 11:41:13 -0800

    clean up Sun

--- Sources/Sun.swift
+++ Sources/Sun.swift
@@ -1,54 +1,6 @@
 
 import Foundation
 
-class planet {
-    var name: String
-    var mass: Double
-    var distanceToSun: Double
-    var gravity: Double
-    var age: Double
-
-    init(name: String, mass: Double, distanceToSun: Double, gravity: Double, age: Double) {
-        self.name = name
-        self.mass = mass
-        self.distanceToSun = distanceToSun
-        self.gravity = gravity
-        self.age = age
-    }
-}
-
-class earth: planet {
-
-    init() {
-        super.init(
-            name:           "Earth", 
-            mass:           1.989e30,   // kg, 
-            distanceToSun:  149.6e6,    // km
-            gravity:        274.0,      // m/s²
-            age:            4.603e9     // years
-        )
-    }
-
-    func getAge() -> Double {
-        return super.age
-    }
-}
-
-
-let Mars_to_Earth_km    =  54605041.92  //km
-let rocket_speed        =  20000.00     // km/h
-var time_to_travel = Mars_to_Earth_km / rocket_speed
-print("Time to travel from Mars to Earth is \(time_to_travel/24) days")
-
-
-
-
-let Earth = earth()
-print(" Hello, the Earth  \(Earth.getAge() ) years old.")
-
-
-
-
 class Sun {
 
     // Constants
@@ -63,3 +15,53 @@ class Sun {
             //self.age             = age
         }
 }
+
+
+
+// class planet {
+//     var name: String
+//     var mass: Double
+//     var distanceToSun: Double
+//     var gravity: Double
+//     var age: Double
+
+//     init(name: String, mass: Double, distanceToSun: Double, gravity: Double, age: Double) {
+//         self.name = name
+//         self.mass = mass
+//         self.distanceToSun = distanceToSun
+//         self.gravity = gravity
+//         self.age = age
+//     }
+// }
+
+// class earth: planet {
+
+//     init() {
+//         super.init(
+//             name:           "Earth", 
+//             mass:           1.989e30,   // kg, 
+//             distanceToSun:  149.6e6,    // km
+//             gravity:        274.0,      // m/s²
+//             age:            4.603e9     // years
+//         )
+//     }
+
+//     func getAge() -> Double {
+//         return super.age
+//     }
+// }
+
+
+// let Mars_to_Earth_km    =  54605041.92  //km
+// let rocket_speed        =  20000.00     // km/h
+// var time_to_travel = Mars_to_Earth_km / rocket_speed
+// print("Time to travel from Mars to Earth is \(time_to_travel/24) days")
+
+
+
+
+// let Earth = earth()
+// print(" Hello, the Earth  \(Earth.getAge() ) years old.")
+
+
+

7301ea90a232c8abcf95b00b9ec6f0245e59d039
Uki D. Lucas <UkiDLucas@gmail.com>
2024-01-04 06:32:32 -0800

    Added parsec

---
+++ Sources/units.swift
@@ -0,0 +1,11 @@
+
+/** 
+1 parsec = 3.08567758 × 10^16 meters 
+or 3.26156 light-years 
+or 206265 AU
+or 30.857 petameters
+or 19.173 quadrillion miles
+or 3.08567758 × 10^13 kilometers
+or 30.857 trillion km
+*/
+let parsec_AU: Double = 206265

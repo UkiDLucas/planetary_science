643459f794fc9dc956a30ab60ac8c7f624038104
Uki D. Lucas <UkiDLucas@gmail.com>
2024-01-04 08:25:06 -0800

    started on life

---
+++ Sources/life.swift
@@ -0,0 +1,27 @@
+/**
+physiological life:
+- eating
+- metabolizing
+- excreting
+- breathing
+- growing
+- moving
+- responsive to external stimuli
+exceptions: viruses, prions, cities, vehicles, etc.
+
+metabolic:
+- consumes energy
+- exchanges materials with environment without changing form
+exception: fire
+
+biochemical:
+- contains reproducible hereditary information
+exceptions: viruses, prions (misfolded protein),
+
+thermodynamic:
+- second law of thermodynamics (enthopy always increases)
+- self-contained system that takes energy to create order locally
+
+https://www.coursera.org/learn/life-on-other-planets/lecture/dVipL/introduction
+
+*/

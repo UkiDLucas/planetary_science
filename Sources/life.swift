e81336f56be9a39e03f3c8026d649aefc0140ce1
Uki D. Lucas <UkiDLucas@gmail.com>
2024-01-05 05:55:25 -0800

    elements

--- Sources/life.swift
+++ Sources/life.swift
@@ -1,4 +1,7 @@
 /**
+https://www.coursera.org/learn/life-on-other-planets/lecture/dVipL/introduction
+
+
 physiological life:
 - eating
 - metabolizing
@@ -22,6 +25,42 @@ thermodynamic:
 - second law of thermodynamics (enthopy always increases)
 - self-contained system that takes energy to create order locally
 
-https://www.coursera.org/learn/life-on-other-planets/lecture/dVipL/introduction
+Genetic (Darwinian)
+- capable of undergoing evolution
+
+example fire
+
+building blocks of life:
+- lipids (hydrophilic, hydrophobic membranes) storage of energy
+- 20 amino acids 
+    (proteins, enzymes, amino groupNH2, carboxyl group COOH, R group)
+    provide structure, catalyze reactions, transport molecules,
+- nucleic acids (DNA, RNA) Nucleotides
+    C-N-H2, C=N-C-C
+- carbohydrates (sugars) 
+    C6H12O6 (glucose) energy storage and transport 
+    H-C-OH, C=O
+
+Common elements:
+- hydrogen
+- helium
+- carbon
+- oxygen
+- neon
+- magnesium
+- silicon
+- calcium
+- sulfur
+- iron
+- nitrogen 
+- phosphorus
+Rare elements:
+
+- gold
+- platinum
+- zirconium
+- lithium
+- berillium
+- boron
 
 */

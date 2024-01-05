fa8dc3abb7eb6207dcc1d7a307ab60b12f5820a4
Uki D. Lucas <UkiDLucas@gmail.com>
2024-01-05 06:37:19 -0800

    Temperature and Luminocity

---
+++ Sources/temperature.swift
@@ -0,0 +1,35 @@
+
+/**
+Temperature scales:
+
+- Fahrenheit : C=5/9(F-32)
+    water boil 212F, 
+    freeze 32F
+    absolute zero -459.67F
+- Celsius    : F=9/5C+32
+    water boil 100C, 
+    freeze 0C
+    absolute zero -273.15C
+- Kelvin     : K=C+273.15
+    water boil 373.15K, 
+    freeze 273.15K
+    absolute zero 0K
+
+
+Luminocity: total energy emitted per unit of time
+Watts: Joules per second, unit of luminocity
+Sun luminocity: 3.828e26 W
+
+
+Stefan-Boltzmann law: 
+    L = 4πR^2σT^4
+    L = luminocity
+    R = radius
+    σ = Stefan-Boltzmann constant
+    T = temperature
+
+*/
+
+
+
+*/

49b2c398e439d1498c3af43740ca03917e52cca9
Uki D. Lucas <UkiDLucas@gmail.com>
2024-01-06 11:56:41 -0800

    Info about MilkyWay

--- Sources/milky_way_galaxy/MilkyWay.swift
+++ Sources/milky_way_galaxy/MilkyWay.swift
@@ -1,2 +1,19 @@
 
 let diameter_parsec = 30000.0
+
+/**
+The Milky Way galaxy
+ is estimated to have a diameter of at least 26,800 parsecs, 
+ or 87,400 light-years. The Milky Way
+ is a barred spiral galaxy 
+ that is about 1,000 light-years thick at the spiral arms, 
+ but thicker at the bulge
+
+ The Milky Way's disk is about 40,000 parsecs across, 
+ and the Sun is about 8,000 parsecs from the center. 
+ However, researchers disagree on the exact value of the distance 
+ from the Sun to the Galactic Center
+. 
+The Milky Way is about 1,000,000,000,000,000,000 kilometers across, 
+which is about 100,000 light years. 
+*/
